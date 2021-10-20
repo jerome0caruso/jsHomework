fdefeffc3042a34ac1027a3e59ca3c0141a78bd5
Jerome <jerome0caruso@gmail.com>
2021-10-20 08:29:31 -0500

    finished homework

---
+++ day2/script.js
@@ -0,0 +1,31 @@
+// // Exercise # 1
+
+const ls = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const l2 = [1, 2, 3, 4, 6, 7, 8, 9]
+
+function firstNonConsecutive(arr) {
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] - 1 !== arr[i-1]) {
+            return arr[i-1];
+        }
+    }
+    return -1;
+}
+
+console.log(firstNonConsecutive(l2))
+
+
+
+// Exercise # 2
+
+l1 = [10, 12, -9, 3, -1, 0, 15]; 
+l2 = [3, 5, 7, 9, -10, 2, -22, -1]; 
+l3 = [-3, -5, -7, -9, -10, -2, -22, -1]; 
+
+const myPostiveAdder = (ls) => {
+    return ls.reduce((acc, num) => {
+        if(num > 0) acc += num;
+        return acc;
+    }, 0);
+}
+console.log(myPostiveAdder(l2))
