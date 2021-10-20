e14f5906abf9b115c5dd5e8f8e2d846216b94e25
Jerome <jerome0caruso@gmail.com>
2021-10-20 09:16:15 -0500

    added another option

--- day2/script.js
+++ day2/script.js
@@ -14,10 +14,22 @@ function firstNonConsecutive(arr) {
 
 console.log(firstNonConsecutive(l2))
 
+// Exercise # 1.2 Recursive
+function firstNonConsecutive2(arr, counter = 1) {
+    if(counter == arr.length) {
+        return -1
+    }
+    if(arr[counter] - 1 !== arr[counter - 1]) {
+        return arr[counter-1]; 
+    }
+    return firstNonConsecutive2(arr, counter = counter + 1)
+}
+
+console.log(firstNonConsecutive2(l2))
+
 
 
 // Exercise # 2
-
 l1 = [10, 12, -9, 3, -1, 0, 15]; 
 l2 = [3, 5, 7, 9, -10, 2, -22, -1]; 
 l3 = [-3, -5, -7, -9, -10, -2, -22, -1]; 
